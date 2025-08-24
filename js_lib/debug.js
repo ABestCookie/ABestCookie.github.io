47d3d7a881954457f898dbd0ca4dd7ab3a6f52f9
cookie <151016914+ABestCookie@users.noreply.github.com>
2025-08-24 15:48:13 +0800

    Update debug.js

--- js_lib/debug.js
+++ js_lib/debug.js
@@ -12,8 +12,12 @@ window.onload = async function () {
 
   // SHA-256 驗證
   const correctHash = "0ea8f6b3e97f3920ebc96cb8c9a0a5a93d65c5cf2938fda3a9a520fdfbff7c4f";
+  console.log("debug:", debug, "password:", password);
+
   if (debug && debug.toLowerCase() === "true" && password) {
     const inputHash = await sha256(password);
+    console.log("inputHash:", inputHash);
+    console.log("correctHash:", correctHash);
     if (inputHash === correctHash) {
       var s = document.createElement('script');
       s.src = 'https://cdn.jsdelivr.net/npm/eruda';
@@ -23,7 +27,7 @@ window.onload = async function () {
         console.log("Eruda loaded!");
       };
     } else {
-        
+      alert("密碼錯誤或 hash 不符\ninputHash: " + inputHash + "\ncorrectHash: " + correctHash);
       console.warn("密碼錯誤或 hash 不符");
     }
   }
