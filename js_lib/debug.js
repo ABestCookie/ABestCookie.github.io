3fc7f5f1e6e23513ac5f9cc6dcec24502bd75663
cookie <151016914+ABestCookie@users.noreply.github.com>
2025-08-24 16:14:16 +0800

    Update debug.js

--- js_lib/debug.js
+++ js_lib/debug.js
@@ -5,19 +5,33 @@ async function sha256(message) {
   return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 }
 
-window.onload = async function () {
-  const url = new URL(window.location.origin + window.location.search);
-  const debug = url.searchParams.get("debug");
-  const password = url.searchParams.get("password");
+// 取得 query string，不論是在 search 或 hash
+function getQueryParams() {
+  let paramsStr = '';
+  if (window.location.search.length > 1) {
+    paramsStr = window.location.search.substring(1);
+  } else if (window.location.hash.indexOf('?') !== -1) {
+    paramsStr = window.location.hash.split('?')[1];
+    // hash 可能還有 /README 之類，剪掉
+    if (paramsStr.indexOf('#') !== -1) paramsStr = paramsStr.split('#')[0];
+    if (paramsStr.indexOf('/') !== -1) paramsStr = paramsStr.split('/')[0];
+  }
+  const params = new URLSearchParams(paramsStr);
+  return {
+    debug: params.get('debug'),
+    password: params.get('password')
+  };
+}
 
-  // SHA-256 驗證
+window.addEventListener('load', async function () {
+  const { debug, password } = getQueryParams();
+
   const correctHash = "0ea8f6b3e97f3920ebc96cb8c9a0a5a93d65c5cf2938fda3a9a520fdfbff7c4f";
   console.log("debug:", debug, "password:", password);
 
   if (debug && debug.toLowerCase() === "true" && password) {
     const inputHash = await sha256(password);
     console.log("inputHash:", inputHash);
-    console.log("correctHash:", correctHash);
     if (inputHash === correctHash) {
       var s = document.createElement('script');
       s.src = 'https://cdn.jsdelivr.net/npm/eruda';
@@ -31,4 +45,4 @@ window.onload = async function () {
       console.warn("密碼錯誤或 hash 不符");
     }
   }
-};
+});
